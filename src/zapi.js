48b9c91149c2a36901d4883193dd374f4a048f6a
maraferpaga-hash <maraferpa.ga@gmail.com>
2026-05-07 01:20:25 -0700

    debug: loga credenciais Z-API antes do envio

--- src/zapi.js
+++ src/zapi.js
@@ -3,6 +3,12 @@ const axios = require('axios');
 async function enviarMensagem(phone, texto) {
   const url = `https://api.z-api.io/instances/${process.env.ZAPI_INSTANCE_ID}/token/${process.env.ZAPI_TOKEN}/send-text`;
 
+  console.log('[ZAPI DEBUG] URL:', url);
+  console.log('[ZAPI DEBUG] Phone:', phone);
+  console.log('[ZAPI DEBUG] INSTANCE_ID:', process.env.ZAPI_INSTANCE_ID);
+  console.log('[ZAPI DEBUG] TOKEN:', process.env.ZAPI_TOKEN?.slice(0, 8) + '...');
+  console.log('[ZAPI DEBUG] CLIENT_TOKEN:', process.env.ZAPI_CLIENT_TOKEN?.slice(0, 8) + '...');
+
   await axios.post(
     url,
     { phone: phone, message: texto },
